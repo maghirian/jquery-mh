b744f001ae1839f7f5f5e18b49db0bb0eaecc4d0
maghirian <maghirian@gmail.com>
2020-02-10 08:59:09 -0500

    sync files

--- custom.js
+++ custom.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function() {
-    $("html").click(function() {alert("Hello, world!");});
+    $("button").click(function() {
+        // alert("Hello, world!");
+        $("button").css("color", "red");});
 });
 
